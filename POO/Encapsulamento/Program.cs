14af80a1e20885e96f3764a19ee95a6d5c53d95a
horizonNEvent <dibu695@gmail.com>
2025-06-26 11:28:21 -0300

    desafio 2

--- POO/Encapsulamento/Program.cs
+++ POO/Encapsulamento/Program.cs
@@ -1,3 +1,7 @@
-﻿Avaliacao avaliacao = new Avaliacao("Carla Silva");
-avaliacao.AtribuirNota(11);   // inválido
-avaliacao.AtribuirNota(8.5);  // válido
+﻿// Avaliacao avaliacao = new Avaliacao("Carla Silva");
+// avaliacao.AtribuirNota(11);   // inválido
+// avaliacao.AtribuirNota(8.5);  // válido
+
+Paciente paciente = new Paciente("Luiz Costa", 42);
+HistoricoMedico historico = new HistoricoMedico("XPT-9987");
+historico.ExibirCodigo();
